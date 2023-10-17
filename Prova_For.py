ed398ad23376853171684463e6d60b50819e83cf
Guilherme Marzano <guilhermemarzano@gmail.com>
2023-10-17 00:21:33 -0300

    First commit- file creating and demand commented

---
+++ Prova_For.py
@@ -0,0 +1,7 @@
+#Aluno:Guilherme Marzano
+#Desenvolver um programa que leia o nome, idade e sexo de 5 pessoas. No final do programa exibir:
+#A média de idade de todo o grupo
+#Nome da pessoa mais velha
+#Quantas pessoas têm menos de 20 anos
+#Quantas pessoas tem mais de 40 anos
+#Qual o sexo da pessoa mais nova
